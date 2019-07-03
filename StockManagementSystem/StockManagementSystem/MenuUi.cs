956e1c4e4063861cbd21fc5d94288fbb5e0ecc29
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-03 11:32:05 +0600

    Project: 1, Layer Model Folders is added

--- StockManagementSystem/StockManagementSystem/MenuUi.cs
+++ StockManagementSystem/StockManagementSystem/MenuUi.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace StockManagementSystem
 {
-    public partial class Form1 : Form
+    public partial class MenuUi : Form
     {
-        public Form1()
+        public MenuUi()
         {
             InitializeComponent();
         }
