494f73546a94b7734f149916923fd7f395c27f96
TUmi Koi <yourteacher@gmail.com>
2019-07-04 15:24:23 +0600

    Merging with TUHIN

--- StockManagementSystem/StockManagementSystem/Program.cs
+++ StockManagementSystem/StockManagementSystem/Program.cs
@@ -17,6 +17,8 @@ namespace StockManagementSystem
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MenuUi());
+            //Application.Run(new CategoryUi());
+            //Application.Run(new CompanyUi());
         }
     }
 }
