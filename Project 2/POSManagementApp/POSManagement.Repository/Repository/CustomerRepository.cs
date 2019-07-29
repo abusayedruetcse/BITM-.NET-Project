df1b98cf0147ecacaf759603c6103cc490646183
Abu Sayed Ahmad <44595937+abusayedruetcse@users.noreply.github.com>
2019-07-29 17:53:49 +0600

    install-package entityframework to UI,Repository & Bootstrap to UI

--- Project 2/POSManagementApp/POSManagement.Repository/Repository/CustomerRepository.cs
+++ Project 2/POSManagementApp/POSManagement.Repository/Repository/CustomerRepository.cs
@@ -1,4 +1,5 @@
-﻿using POSManagementApp.DatabaseContext.DatabaseContext;
+﻿using POSManagement.Models.Models;
+using POSManagementApp.DatabaseContext.DatabaseContext;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace POSManagement.Repository.Repository
     public class CustomerRepository
     {
         SBMSystemDb db;
+        Customer _customer;
         public CustomerRepository()
         {
             db = new SBMSystemDb();
